44681496f1b5e8451477a58034248675adc124ca
Zizheng Yang <32321397+ZizhengYang@users.noreply.github.com>
2023-09-15 13:13:23 -0500

    Add files via upload

--- cnv.py
+++ cnv.py
@@ -1,6 +1,23 @@
-from utils import is_prime
+from utils import exp_pi_i, frac, exp_pi_i_zero, exp_pi_i_plus_one
 
-class c3v():
+class C3v():
 
     def __init__(self):
-        self.dim = [1,1,2]
+        self.elements = ['E', 'C3', 'C3\'', 'sigma_v', 'sigma_v\'', 'sigma_v\'\'']
+        self.irrep_dims = [1,1,2]
+        self.irrep_names = ['A1', 'A2', 'E']
+        self.irreps = []
+        self.irreps.append([1, 1, 1, 1, 1, 1])
+        self.irreps.append([1, 1, 1, -1, -1, -1])
+        epsilon = exp_pi_i(frac(2, 3), +1)
+        irrep_E = []
+        irrep_E.append([[exp_pi_i_plus_one, exp_pi_i_zero], [exp_pi_i_zero, exp_pi_i_plus_one]])
+        irrep_E.append([[epsilon.conjugate(), exp_pi_i_zero], [exp_pi_i_zero, epsilon]])
+        irrep_E.append([[epsilon, exp_pi_i_zero], [exp_pi_i_zero, epsilon.conjugate()]])
+        irrep_E.append([[exp_pi_i_plus_one, exp_pi_i_zero], [exp_pi_i_zero, exp_pi_i_plus_one]])
+        irrep_E.append([[exp_pi_i_plus_one, exp_pi_i_zero], [exp_pi_i_zero, exp_pi_i_plus_one]])
+        irrep_E.append([[exp_pi_i_plus_one, exp_pi_i_zero], [exp_pi_i_zero, exp_pi_i_plus_one]])
+
+
+print(1+1)
+print(C3v().irreps[0])
--- utils.py
+++ utils.py
@@ -19,57 +19,13 @@ class frac():
 
 class exp_pi_i():
 
-    def __init__(self, radian:frac, pm):
+    def __init__(self, radian:frac, multi):
         self.radian = radian
-        self.pm = pm
+        self.multi = multi
 
     def conjugate(self):
         self.radian = self.radian.negative()
-        
-
-# =======================
-# real and compelx number
-# =======================
-
-class real_num():
-
-    def __init__(self, c, sqrts):
-        self.c = c
-        self.sqrts = sqrts
-
-    def calculate(self):
-        return self.c * math.sqrt(self.sqrts)
 
 
-class complex_num():
-
-    def __init__(self, real_part, complex_part):
-        # Both real part and complex part are arrays of real_num()
-        # Array element representing addition relation
-        self.real_part = real_part
-        self.complex_part = complex_part
-
-
-def real_multiply(x1: real_num, x2: real_num) -> real_num:
-    x1_temp = x1.sqrts
-    x2_temp = x2.sqrts
-    extract = 1
-    for sqrt1 in x1.sqrts:
-        for sqrt2 in x2_temp:
-            if sqrt1==sqrt2:
-                x1_temp.remove(sqrt1)
-                x2_temp.remove(sqrt2)
-                extract *= sqrt2
-                break
-    return real_num(x1.c*x2.c*extract, x1_temp+x2_temp)
-
-
-def complex_multiply(x1: complex_num, x2: complex_num) -> complex_num:
-    return complex_num()
-
-
-def is_prime(num):
-    for i in range(2, num):
-        if (num % i) == 0:
-            return False
-    return True
+exp_pi_i_zero = exp_pi_i(0, 0)
+exp_pi_i_plus_one = exp_pi_i(0, 1)
